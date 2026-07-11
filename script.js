a3246dd74488b65978cb1f11029d9ac58c5ecbd0
MaliroLiro <emailheiro@gmail.com>
2026-07-10 23:54:31 -0300

    adiciona botao de excluir item da lista e inicio de armazenamento de dados em array para futuro localStorage

--- script.js
+++ script.js
@@ -2,66 +2,89 @@
 const inputDecisao = document.getElementById("inputDecisao");
 const listaDecisoes = document.getElementById("listaDecisoes");
 
+const arrayDecisoes = [];
+
 //funcao principal
-function decidido(){
+function decidido() {
+  //cria um card de prós e contras
+  const texto = inputDecisao.value;
+  const cardMain = document.createElement("div");
+  const cardPros = document.createElement("div");
+  const cardCons = document.createElement("div");
+  const titulo = document.createElement("h2");
+  const listas = document.createElement("div");
+  titulo.textContent = texto;
+  cardMain.classList.add("cardMain");
 
-    //cria um card de prós e contras
-    const texto = inputDecisao.value;
-    const cardMain = document.createElement("div")
-    const cardPros = document.createElement("div");
-    const cardCons = document.createElement("div");
-    const titulo = document.createElement("h2");
-    const listas = document.createElement("div");
-    titulo.textContent = texto;
-    cardMain.classList.add("cardMain");
+  //adiciona a decisão ao array de decisões
+  const indice = arrayDecisoes.length;
+  arrayDecisoes.push({
+    titulo: texto,
+    pros: [],
+    cons: [],
+  });
 
-    //variaveis da interface de prós
-    const pros = document.createElement("h3");
-    const listaPros = document.createElement("ul");
-    const inputPros = document.createElement("input");
-    const botaoPros = document.createElement("button");
-    
-    //funcionalidade da interface de prós
-    listaPros.classList.add("listaPros")
-    pros.textContent = "Prós";
-    inputPros.type = "text";
-    botaoPros.textContent = "+ Pró";
-    botaoPros.addEventListener("click", () =>{
-        const texto = inputPros.value.trim();
-        if (texto === "") return;
-        const item = document.createElement("li");
-        item.textContent = texto;
-        listaPros.appendChild(item);
-        inputPros.value = "";
+  //variaveis da interface de prós
+  const pros = document.createElement("h3");
+  const listaPros = document.createElement("ul");
+  const inputPros = document.createElement("input");
+  const botaoPros = document.createElement("button");
+
+  //funcionalidade da interface de prós
+  listaPros.classList.add("listaPros");
+  pros.textContent = "Prós";
+  inputPros.type = "text";
+  botaoPros.textContent = "+ Pró";
+  botaoPros.addEventListener("click", () => {
+    const texto = inputPros.value.trim();
+    if (texto === "") return;
+    const item = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Excluir";
+    item.textContent = texto;
+    item.appendChild(deleteButton);
+    listaPros.appendChild(item);
+    inputPros.value = "";
+    deleteButton.addEventListener("click", () => {
+      listaPros.removeChild(item);
     });
+    arrayDecisoes[indice].pros.push(texto);
+    console.log(arrayDecisoes);
+  });
 
-    //variáveis da interface de contras
-    const cons = document.createElement("h3");
-    const listaCons = document.createElement("ul");
-    const inputCons = document.createElement("input");
-    const botaoCons = document.createElement("button");
-    
+  //variáveis da interface de contras
+  const cons = document.createElement("h3");
+  const listaCons = document.createElement("ul");
+  const inputCons = document.createElement("input");
+  const botaoCons = document.createElement("button");
 
-    //funcionalidade da interface de contras
-    listaCons.classList.add("listaCons")
-    cons.textContent = "Contras";
-    inputCons.type = "text";
-    botaoCons.textContent = "+ Contra";
-    botaoCons.addEventListener("click", () =>{
-        const texto = inputCons.value.trim();
-        if (texto === "") return;
-        const item = document.createElement("li");
-        item.textContent = texto;
-        listaCons.appendChild(item);
-        inputCons.value = "";
+  //funcionalidade da interface de contras
+  listaCons.classList.add("listaCons");
+  cons.textContent = "Contras";
+  inputCons.type = "text";
+  botaoCons.textContent = "+ Contra";
+  botaoCons.addEventListener("click", () => {
+    const texto = inputCons.value.trim();
+    if (texto === "") return;
+    const item = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Excluir";
+    item.textContent = texto;
+    item.appendChild(deleteButton);
+    listaCons.appendChild(item);
+    inputCons.value = "";
+    deleteButton.addEventListener("click", () => {
+      listaCons.removeChild(item);
     });
+    arrayDecisoes[indice].cons.push(texto);
+    console.log(arrayDecisoes);
+  });
 
-    //insere elementos no DOM e limpa input inicial
-    cardPros.append(pros, inputPros, botaoPros, listaPros);
-    cardCons.append(cons, inputCons, botaoCons, listaCons);
-    cardMain.append(cardPros, cardCons);
-    listaDecisoes.append(titulo, cardMain);
+  //insere elementos no DOM e limpa input inicial
+  cardPros.append(pros, inputPros, botaoPros, listaPros);
+  cardCons.append(cons, inputCons, botaoCons, listaCons);
+  cardMain.append(cardPros, cardCons);
+  listaDecisoes.append(titulo, cardMain);
 
-    inputDecisao.value = "";
+  inputDecisao.value = "";
 }
-
