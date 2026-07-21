f26b67ce20c01266568326def61adda1ee26ff49
MaliroLiro <emailheiro@gmail.com>
2026-07-20 22:52:59 -0300

    refatoração simples

--- script.js
+++ script.js
@@ -15,48 +15,52 @@ function salvar() {
   localStorage.setItem("decisoes", JSON.stringify(arrayDecisoes));
 }
 
+//renderiza a tela com as decisões
 function renderizarTela() {
+
   listaDecisoes.innerHTML = "";
   arrayDecisoes.forEach((decisao, indiceDecisao) => {
+
     //variáveis da interface de decisão
     const cardMain = document.createElement("div");
     const cardPros = document.createElement("div");
     const cardCons = document.createElement("div");
-    const titulo = document.createElement("h2");
+    const tituloDecisao = document.createElement("h2");
     const listas = document.createElement("div");
 
     //variaveis da interface de prós
-    const pros = document.createElement("h3");
+    const tituloPros = document.createElement("h3");
     const listaPros = document.createElement("ul");
     const inputPros = document.createElement("input");
     const botaoPros = document.createElement("button");
 
     //variáveis da interface de contras
-    const cons = document.createElement("h3");
+    const tituloCons = document.createElement("h3");
     const listaCons = document.createElement("ul");
     const inputCons = document.createElement("input");
     const botaoCons = document.createElement("button");
 
     //funcionalidade da interface de prós
     listaPros.classList.add("listaPros");
-    pros.textContent = "Prós";
+    tituloPros.textContent = "Prós";
     inputPros.type = "text";
     botaoPros.textContent = "+ Pró";
     botaoPros.addEventListener("click", () => {
-      const texto = inputPros.value.trim();
-      addPro(indiceDecisao, texto);
+      const textoPro = inputPros.value.trim();
+      addPro(indiceDecisao, textoPro);
     });
 
     //funcionalidade da interface de contras
     listaCons.classList.add("listaCons");
-    cons.textContent = "Contras";
+    tituloCons.textContent = "Contras";
     inputCons.type = "text";
     botaoCons.textContent = "+ Contra";
     botaoCons.addEventListener("click", () => {
-      const texto = inputCons.value.trim();
-      addCon(indiceDecisao, texto);
+      const textoCon = inputCons.value.trim();
+      addCon(indiceDecisao, textoCon);
     });
 
+    //renderiza os prós da decisão
     decisao.pros.forEach((pro, indexPro) => {
       const item = document.createElement("li");
       const deleteButton = document.createElement("button");
@@ -69,6 +73,7 @@ function renderizarTela() {
       });
     });
 
+    //renderiza os contras da decisão
     decisao.cons.forEach((con, indexCons) => {
       const item = document.createElement("li");
       const deleteButton = document.createElement("button");
@@ -81,37 +86,36 @@ function renderizarTela() {
       });
     });
 
-    titulo.textContent = decisao.titulo;
+    //adiciona classes e conteúdo aos elementos
+    tituloDecisao.textContent = decisao.titulo;
+    tituloPros.classList.add("tituloPros");
+    tituloCons.classList.add("tituloCons");
     cardMain.classList.add("cardMain");
 
     //insere elementos no DOM
-    cardPros.append(pros, inputPros, botaoPros, listaPros);
-    cardCons.append(cons, inputCons, botaoCons, listaCons);
+    cardPros.append(tituloPros, inputPros, botaoPros, listaPros);
+    cardCons.append(tituloCons, inputCons, botaoCons, listaCons);
     cardMain.append(cardPros, cardCons);
-    listaDecisoes.append(titulo, cardMain);
+    listaDecisoes.append(tituloDecisao, cardMain);
   });
 }
 
-//função para adicionar prós à lista
-function addPro(indexDecisao, texto) {
-  if (texto === "") {
-    alert("Digite um pró válido!");
-    return;
-  }
-  arrayDecisoes[indexDecisao].pros.push(texto);
+//funções para adicionar prós e contras na lista
+function addPro(indexDecisao, textoPro) {
+  if (textoPro === "") return;
+  arrayDecisoes[indexDecisao].pros.push(textoPro);
   salvar();
   renderizarTela();
 }
 
-//função para adicionar contras à lista
-function addCon(indexDecisao, texto) {
-  if (texto === "") return;
-  
-  arrayDecisoes[indexDecisao].cons.push(texto);
+function addCon(indexDecisao, textoCon) {
+  if (textoCon === "") return;
+  arrayDecisoes[indexDecisao].cons.push(textoCon);
   salvar();
   renderizarTela();
 }
 
+//funções para remover prós e contras da lista
 function rmvPro(indexDecisao, indexPro) {
   arrayDecisoes[indexDecisao].pros.splice(indexPro, 1);
   salvar();
@@ -124,17 +128,18 @@ function rmvCon(indexDecisao, indexCon) {
   renderizarTela();
 }
 
-//funcao que cria uma decisao
+//função que cria uma decisao
 function criarDecisao() {
-  const texto = inputDecisao.value.trim();
-  if (texto === "") return;
+  const textoDecisao = inputDecisao.value.trim();
+  if (textoDecisao === "") return;
 
   arrayDecisoes.push({
-    titulo: texto,
+    titulo: textoDecisao,
     pros: [],
     cons: [],
   });
 
+  //salva e renderiza a tela
   salvar();
   renderizarTela();
   inputDecisao.value = "";
